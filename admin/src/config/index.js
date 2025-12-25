8f6aa6ff5dea3a8b835a4c941509c8421c936367
dzctl1820 <3186807685@qq.com>
2025-12-25 12:58:02 +0800

    修改管理端地址

--- admin/src/config/index.js
+++ admin/src/config/index.js
@@ -1,13 +1,9 @@
 // 应用配置
 const config = {
   // API基础URL（用于API请求）
-<<<<<<< HEAD
-   apiBaseURL: 'http://162.14.106.139/api',
-=======
-   apiBaseURL: 'http://162.14.106.139:8080/api',
->>>>>>> 634f3995b008c898b0cd0b79582d27f8984886f7
+   apiBaseURL: 'https://filmingfilmsinyaan.com/api',
   // 文件服务基础URL（用于访问静态文件，不包含/api）
-   fileBaseURL: 'http://162.14.106.139/api'
+   fileBaseURL: 'https://filmingfilmsinyaan.com/api'
 }
 
 export default config
